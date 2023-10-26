54a115b98cf564ceeacecc5e42077272777b2adc
John Karmozyn <hughkarmozyn@icloud.com>
2023-10-26 10:07:35 -0600

    commits

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,21 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         Stack s = new Stack();
-        System.out.println(s.isEmpty());
-        s.push(2);
-        System.out.println(s.peek());
-        s.push(3);
-        System.out.println(s.peek());
-        s.push(4);
-        System.out.println(s.peek());
-        s.pop();
-        System.out.println(s.peek());
-        s.pop();
-        System.out.println(s.peek());
-        s.pop();
-        System.out.println(s.isEmpty());
-        s.push(10);
-        System.out.println(s.isEmpty());
+        System.out.println(s.isEmpty()); // returns false
+        s.push(2); //adds 2
+        System.out.println(s.peek()); // souts 2
+        s.push(3); // adds 3
+        System.out.println(s.peek()); /// souts 3
+        s.push(4); // adds 4
+        System.out.println(s.peek()); // souts 4
+        s.pop(); // removes 4
+        System.out.println(s.peek()); //souts 3
+        s.pop(); // removes 3
+        System.out.println(s.peek()); // sout 2
+        s.pop(); // removes 2
+        System.out.println(s.isEmpty()); //returns false
+        s.push(10); // adds 10
+        System.out.println(s.isEmpty());// returns true
+        System.out.println(s.getSize());//returns the size
 
 
 
